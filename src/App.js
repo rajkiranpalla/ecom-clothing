5f025349eb729fc5d820e4a44db20608f13f4ee4
rpalla <rpalla@manh.com>
2021-08-16 22:58:32 +0530

    added basic routing with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import { Homepage } from './pages/homepage/Homepage.component';
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Route exact path='/' component={Homepage} />
+      <Route exact path='/hats' component={HatsPage} />
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,10 @@
 
 import './menu-item.styles.scss';
+import {withRouter} from 'react-router-dom';
 
-export const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size ,linkUrl, history, match }) => (
+    <div className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className = 'background-image' style={{
         backgroundImage: ` url(${imageUrl})`
     }}>
@@ -13,3 +15,5 @@ export const MenuItem = ({ title, imageUrl, size }) => (
         </div>
     </div>
 )
+
+export default withRouter(MenuItem);
