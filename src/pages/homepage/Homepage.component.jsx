dc5ba8ca067971fdd16ab6e56dd12dc8d40addfc
rpalla <rpalla@manh.com>
2021-08-16 18:51:53 +0530

    added homepage and menuitem components

---
+++ src/pages/homepage/Homepage.component.jsx
@@ -0,0 +1,8 @@
+
+import './Homepage.styles.scss';
+import {DirectoryMenu} from '../../components/directory-menu/directory-menu.component';
+export const Homepage = () => (
+    <div className ='homepage'>
+       <DirectoryMenu/>
+    </div>
+)
