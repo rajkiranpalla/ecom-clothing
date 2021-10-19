7bd76c13cb08f41fd99ba008d9e46aee2ebcbf1c
rpalla <rpalla@manh.com>
2021-10-19 12:21:24 +0530

    added header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,12 +4,19 @@ import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 const Header = () => (
- <div className="header">
-
-  <Link to='/' > homepage</Link>
-  <Link to='/shop'>shoppage</Link>
-
- </div>
+    <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
+      </Link>
+      <Link className='option' to='/shop'>
+        CONTACT
+      </Link>
+    </div>
+  </div>
 
 );
 
